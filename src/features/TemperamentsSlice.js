977583e9f99184d529195e761ca8a1aedd6a1d58
Arthaz1245 <sergiomendozap96@gmail.com>
2023-04-10 18:10:34 -0500

    ho

--- src/features/TemperamentsSlice.js
+++ src/features/TemperamentsSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import axios from "axios";
-const TEMPERAMENTS_URL = "http://localhost:5000/temperaments";
+const TEMPERAMENTS_URL = "backpidogs.onrender.com/temperaments";
+// const TEMPERAMENTS_URL = "http://localhost:5000/temperaments";
 
 const initialState = {
   temperaments: [],
